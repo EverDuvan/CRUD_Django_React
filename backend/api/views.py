ac2d90db0e4c3cbb7f931e253294df87b2350b17
EverDuvan <everduvanhdez@gmail.com>
2024-02-10 14:18:04 -0500

    update

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from api.models import Blog
+from api.serializers import BlogSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def getBlogs(request):
+    blogs = Blog.objects.all()
+    serializer = BlogSerializer(blogs, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def postBlog(request):
+    data = request.data
+    serializer = BlogSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['PUT'])
+def putBlog(request, pk):
+    data = request.data
+    # blog = Blog.objects.get(id=pk) 
+    blog = Blog.objects.filter(id=pk).first()
+    serializer = BlogSerializer(instance=blog, data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['DELETE'])
+def deleteBlog(request, pk):
+    blog = Blog.objects.get(id=pk)
+    blog.delete()
+    return Response('Blog deleted')
